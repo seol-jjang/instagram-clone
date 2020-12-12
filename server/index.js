6167afe5d4601e4079fe8f903eb66575ee7c00d8
seoljjang <seoljjang00@gmail.com>
2020-12-12 18:56:06 +0900

    로그인기능, 토큰 생성

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User");
 
@@ -20,6 +21,7 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -31,6 +33,36 @@ app.post("/register", (req, res) => {
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일이 데이터베이스에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message:
+          "입력한 사용자 이름을 사용하는 계정을 찾을 수 없습니다. 사용자 이름을 확인하고 다시 시도하세요."
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      //확인했다면 비밀번호가 일치하는 지 확인
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "잘못된 비밀번호입니다. 다시 확인하세요."
+        });
+      }
+      //일치한다면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -19,7 +20,8 @@ const userSchema = mongoose.Schema({
   },
   nickname: {
     type: String,
-    maxlength: 50
+    maxlength: 50,
+    unique: 1
   },
   role: {
     type: Number,
@@ -46,8 +48,29 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  const user = this;
+
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
