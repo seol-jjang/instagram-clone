1171affa0b17f50a122f08789717cfc321ae09dd
seoljjang <seoljjang00@gmail.com>
2021-01-25 18:16:37 +0900

    이미지 서버 변경 및 자잘한 수정

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -6,18 +6,12 @@ import { palette } from "./Theme";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
-
-    @font-face {
-    font-family: 'NanumSquareRound';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
+    
     body {
         margin: 0;
         box-sizing: border-box;
-        font-family: -apple-system, BlinkMacSystemFont, 'NanumSquareRound',
-            "맑은 고딕", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+        font-family: -apple-system, BlinkMacSystemFont,
+            "Segoe UI", "맑은 고딕", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
             "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
